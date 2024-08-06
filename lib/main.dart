417eba9cdcdfb5f97c1fd19f6cc60ca492f23ece
ranad4508 <ranad4508@gmail.com>
2024-08-06 20:42:29 +0545

    flutter firebase setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-// Entry point of flutter app
-void main() {
+// Entry point of flutteFuture<void>p
+Future<void> main() async {
   //TODO: Add widgets binding
   //TODO: Init local storage
   //TODO: Await native splash
   //TODO: Initialize firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then(
+      (FirebaseApp value)=>Get.put(AuthenticationRepository()),
+  );
   //TODO: Initialize Authentication
 
+  //Load all the Material Design / Themes / Localizations / Bindings
   runApp(const App());
 }
-
-
